f43312313c4aa7eb1078d7bce00dbf9d8f36051e
Ches613 <zverkovmi@gmail.com>
2023-02-02 15:53:57 +0400

    added lecture 5

---
+++ lect_5/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+//        Lesson_05.Ex001_HashMap.main(args);
+//        Lesson_05.Ex002_HashMapEntry.main(args);
+//        Lesson_05.Ex003_HashMapBoost.main(args);
+//        Lesson_05.Ex004_TreeMap.main(args);
+        Lesson_05.Ex005_LinkedHashMap.main(args);
+//        Lesson_05.Ex006_HashTable.main(args);
+    }
+}
